446aa561c3627c06250ac93ff054210b02d81980
Nils Carlson <nils@nilscarlson.se>
2022-03-02 21:10:40 +0000

    Properly split - unit tests should be possible

--- src/bin/social-blog/main.rs
+++ src/bin/social-blog/main.rs
@@ -1,34 +1,11 @@
-use tonic::{transport::Server, Request, Response, Status};
+use tonic::transport::Server;
 
-use social_blog::blog_server::{Blog, BlogServer};
-use social_blog::{CreatePageRequest, CreatePageResponse};
-use uuid::Uuid;
-
-#[derive(Default)]
-pub struct BlogService {}
-
-#[tonic::async_trait]
-impl Blog for BlogService {
-    async fn create_page(
-        &self,
-        request: Request<CreatePageRequest>,
-    ) -> Result<Response<CreatePageResponse>, Status> {
-        println!("Got a request from {:?}", request.remote_addr());
-
-        let page_uuid = social_blog::Uuid {
-            id: Uuid::new_v4().to_string(),
-        };
-        let reply = social_blog::CreatePageResponse {
-            page_id: Some(page_uuid),
-        };
-        Ok(Response::new(reply))
-    }
-}
+use social_blog::blog_server::BlogServer;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse().unwrap();
-    let blog = BlogService::default();
+    let blog = social_blog::blog_service::BlogService::default();
 
     println!("BlogServer listening on {}", addr);
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,5 @@
 tonic::include_proto!("social_blog");
+
+
+pub mod blog_service;
+pub use blog_service::BlogService;
